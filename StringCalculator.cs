6280aec86553e365fd03ac61ee201f07b5b48e83
Tjalve Aarflot <tjalve.aarflot@ice.net>
2022-01-07 09:46:35 +0100

    Initial commit
    Update settings.json

---
+++ StringCalculator.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        public void Add() {}
+    }
+
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+
+        [Test]
+        public void Add_Test()
+        {
+            var sc = new StringCalculator();
+            sc.Add();
+        }
+    }
+}
